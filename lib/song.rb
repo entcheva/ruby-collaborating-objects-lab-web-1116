899d81b2aaba6ed491523dd85ca21b57d8e20c9d
entcheva <christina.entcheva@gmail.com>
2016-12-03 22:54:17 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,11 @@ class Song
 
   def initialize(name)
     @name = name
-    @artist = artist
   end
 
   def self.new_by_filename(file)
     song = new(file.split(" - ")[1].chomp)
     artisttt = Artist.find_or_create_by_name(file.split(" - ")[0].chomp)
-    song.artist = artisttt
-    artist.add_song(song)
-    song
   end
 
 end
